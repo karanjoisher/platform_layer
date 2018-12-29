b452956ed583368dc46bc773b41c1ec5ba7e5f5d
karanjoisher <karanjoisher@gmail.com>
2018-12-29 17:41:30 +0530

    Added glew to cross check difficult to debug bugs

--- build.bat
+++ build.bat
@@ -5,13 +5,14 @@ python opengl_function_generator.py PLATFORM_WINDOWS .
 IF NOT EXIST ./build mkdir ./build
 cd build
 
-set FLAGS=-DSLOW_BUILD=1 -DDEBUG_BUILD=1 -DPLATFORM_WINDOWS=1
+set FLAGS=-DSLOW_BUILD=1 -DDEBUG_BUILD=1 -DPLATFORM_WINDOWS=1 -DPF_GLEW_ENABLED=0
 
 set CommonCompilerFlags= -MTd -Gm- -nologo -GR- -EHa- -Oi  -W4 -wd4201 -wd4100 -wd4127 -wd4505 -wd4189 -fp:fast  /Z7 
 
-set CommonLinkerFlags=user32.lib Gdi32.lib winmm.lib Ole32.lib openGL32.lib 
+REM set GlewLibrary=/LIBPATH:W:/platform_layer/glew/lib/Win32 glew32s.lib
+set CommonLinkerFlags=%GlewLibrary% user32.lib Gdi32.lib winmm.lib Ole32.lib openGL32.lib 
 
-cl %FLAGS% %CommonCompilerFlags% -Fmapplication.map W:/platform_layer/application.cpp /link %CommonLinkerFlags% -incremental:no
+cl  /I W:/platform_layer/glew/include %FLAGS% %CommonCompilerFlags% -Fmapplication.map W:/platform_layer/application.cpp /link    %CommonLinkerFlags% -incremental:no
 
 cd ..
 
