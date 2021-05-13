4a48a3496caf58351d0acba1440587691e68c26e
Karan Joisher <karanjoisher@gmail.com>
2021-05-13 14:33:06 +0530

    Update release_build.bat

--- release_build.bat
+++ release_build.bat
@@ -1,7 +1,7 @@
 @echo off
 
-IF NOT EXIST ./build mkdir build
-cd build
+if not exist build\. mkdir build
+pushd build
 
 set FLAGS=-DSLOW_BUILD=0 -DDEBUG_BUILD=0 -DPLATFORM_WINDOWS=1 -DPF_GLEW_ENABLED=0 -DHOT_CODE_RELOADABLE=0 -DINPUT_RECORDING_PLAYBACK=0
 
@@ -9,6 +9,6 @@ set CommonCompilerFlags= -MTd -Gm- -nologo -GR- -EHa- -Oi  -W4 -wd4201 -wd4100 -
 
 set CommonLinkerFlags=user32.lib Gdi32.lib winmm.lib Ole32.lib openGL32.lib 
 
-cl %FLAGS% %CommonCompilerFlags% -Fmapplication.map ../application.cpp /link %CommonLinkerFlags% -incremental:no
+cl %FLAGS% %CommonCompilerFlags% -Fmapplication.map ..\application.cpp /link %CommonLinkerFlags% -incremental:no
 
-cd ..
+popd
