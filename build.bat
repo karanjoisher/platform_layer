743ad9e0070ae28bca951fc75bf5462236a0d444
Karan Joisher <karanjoisher@gmail.com>
2021-05-13 14:31:45 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,8 +2,8 @@
 
 REM python opengl_function_generator.py PLATFORM_WINDOWS .
 
-IF NOT EXIST ./build mkdir build
-cd build
+if not exist build\. mkdir build
+pushd build
 
 set FLAGS=-DSLOW_BUILD=0 -DDEBUG_BUILD=0 -DPLATFORM_WINDOWS=1 -DPF_GLEW_ENABLED=0 -DHOT_CODE_RELOADABLE=0 -DINPUT_RECORDING_PLAYBACK=0
 
@@ -12,8 +12,6 @@ set CommonCompilerFlags= -MTd -Gm- -nologo -GR- -EHa- -Oi  -W4 -wd4201 -wd4100 -
 REM set GlewLibrary=/LIBPATH:W:/platform_layer/glew/lib/Win32 glew32s.lib
 set CommonLinkerFlags=%GlewLibrary% user32.lib Gdi32.lib winmm.lib Ole32.lib openGL32.lib 
 
-cl %FLAGS% %CommonCompilerFlags% -Fmapplication.map W:/platform_layer/application.cpp /link    %CommonLinkerFlags% -incremental:no
+cl %FLAGS% %CommonCompilerFlags% -Fmapplication.map ..\application.cpp /link %CommonLinkerFlags% -incremental:no
 
-cd ..
-
-
+popd
